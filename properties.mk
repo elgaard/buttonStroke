9ff7e0951bff38d7b744db0f0a8b858eab5f438e
Niels Elgaard Larsen <elgaard@agol.dk>
2023-03-30 19:42:46 +0200

    properties file

--- properties.mk
+++ properties.mk
@@ -2,4 +2,4 @@ DEVICE = fr235
 SDK_HOME =  /home/$(USER)/src/garmin
 DEPLOY = /media/$(USER)/GARMIN/GARMIN/APPS/
 PRIVATE_KEY = developer_key.der
-BINDIR=/home/XX/XX/bin
+BINDIR=/home/$(USER)/garmin/bin
